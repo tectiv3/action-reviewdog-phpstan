91bcbf37b078d0917201b9cc321dad385fe5c08a
tectiv3 <git@getpanda.me>
2020-12-05 11:40:29 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,6 @@ if [ -n "${GITHUB_WORKSPACE}" ]; then
 fi
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
-
-php /usr/local/bin/phpstan.phar analyse ${INPUT_TARGET_DIRECTORY} --level=max --memory-limit 1G --error-format=raw ${INPUT_ARGS} \
+cd "${INPUT_TARGET_DIRECTORY}"
+./vendor/bin/phpstan analyse --level=max --memory-limit 1G --error-format=raw ${INPUT_ARGS} \
     | reviewdog -name=PHPStan -f=phpstan -reporter=${INPUT_REPORTER} -level=${INPUT_LEVEL} -diff='git diff'
